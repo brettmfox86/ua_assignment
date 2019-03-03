a192b240262e21a1547bd17891b2bb8cd137cc08
bfox6 <bfox6@nd.edu>
2019-03-03 14:18:26 -0500

    Initial Project setup

---
+++ api/views.py
@@ -0,0 +1,31 @@
+contacts = [
+    {
+        'id': '49845a3d-f5ab-4b3a-8e16-965037549d71',
+        'first_name': 'Brett',
+        'last_name': 'Fox',
+        'email_address': 'brettfox@email.com',
+        'phone_number': '5555555555'
+    },
+    {
+        'id': 'e1473de5-dc9d-497b-9541-beff4f9e988e',
+        'first_name': 'John',
+        'last_name': 'Guy',
+        'email_address': 'Johnguy@email.com',
+        'phone_number': '5555555556'
+    },
+    {
+        'id': '958b38bd-827c-4e45-9775-3fa7c547a524',
+        'first_name': 'Jane',
+        'last_name': 'Woman',
+        'email_address': 'Janewoman@email.com',
+        'phone_number': '5555555556'
+    }
+]
+
+# GET - Get a list of contacts, optionally filtering by a search query.
+#     The API must support searching by at least one field (e.g., email address).
+def get_contacts()
+# POST - Create a new contact
+# GET - Get a contact
+# PATCH - Replace a contact's data
+# DELETE - Delete a contact.
